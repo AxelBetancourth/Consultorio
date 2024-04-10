0ed4451e673e5b27889c691ab457beddfef91860
Axel <abetancourth2004@gmail.com>
2024-04-10 16:49:07 -0600

    Creacion del proyecto

---
+++ CapaDatos/BaseDatos/ConsultorioContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos.BaseDatos
+{
+    public class ConsultorioContext : DbContext
+    {
+        public ConsultorioContext() : base("Consultorio")
+        {
+            
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
