1b6fa01bdae85d8c978ea0aef05095345c8e343c
Axel <abetancourth2004@gmail.com>
2024-04-10 19:57:13 -0600

    Capa presentacion de las 3 tablas

---
+++ CapaNegocio/NMedico.cs
@@ -0,0 +1,41 @@
+﻿using CapaDatos;
+using CapaDatos.BaseDatos.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class NMedico
+    {
+        DMedico dMedico;
+
+        public NMedico()
+        {
+            dMedico = new DMedico();
+        }
+
+        public List<Medico> MostrarMedicos()
+        {
+            return dMedico.TodosLosMedicos();
+        }
+        public List<Medico> MedicosActivos()
+        {
+            return dMedico.TodosLosMedicos().Where(c => c.Estado == true).ToList();
+        }
+        public int AgregarMedicos(Medico medico)
+        {
+            return dMedico.GuardarMedicos(medico);
+        }
+        public int EditarMedicos(Medico medico)
+        {
+            return dMedico.GuardarMedicos(medico);
+        }
+        public int EliminarMedicos(int medicoId)
+        {
+            return dMedico.EliminarMedicos(medicoId);
+        }
+    }
+}
---
+++ CapaNegocio/NPaciente.cs
@@ -0,0 +1,41 @@
+﻿using CapaDatos;
+using CapaDatos.BaseDatos.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class NPaciente
+    {
+        private DPaciente dPaciente;
+
+        public NPaciente()
+        {
+            dPaciente = new DPaciente();
+        }
+
+        public List<Paciente> MostrarPacientes()
+        {
+            return dPaciente.TodosLosPacientes();
+        }
+        public List<Paciente> PacientesActivos()
+        {
+            return dPaciente.TodosLosPacientes().Where(c => c.Estado == true).ToList();
+        }
+        public int AgregarPacientes(Paciente paciente)
+        {
+            return dPaciente.GuardarPacientes(paciente);
+        }
+        public int EditarPacientes(Paciente paciente)
+        {
+            return dPaciente.GuardarPacientes(paciente);
+        }
+        public int EliminarPacientes(int pacienteId)
+        {
+            return dPaciente.EliminarPacientes(pacienteId);
+        }
+    }
+}
